914f0f24a3853b41039f6f17ad869ae3c1047ada
RuaCat <chj97610@gmail.com>
2024-12-31 14:44:23 +0900

    캐릭터 프로필 이미지 1차
    Next: 커서에 따라 캐릭터 이미지 변경시키기

--- Assets/3.Script/SelectManager.cs
+++ Assets/3.Script/SelectManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.U2D;
 using UnityEngine.UI;
@@ -9,16 +10,15 @@ public class SelectManager : MonoBehaviour
     public SpriteAtlas spriteAtlas;  // Sprite Atlas를 Inspector에서 할당
     public Image[] image;            // UI Image 컴포넌트
     public GameObject[] cursor;
-    private string[] spriteName = {"11_Mario","12_Luigi","13_Yoshi","14_Peach","15_Daisy","16_Rosalina",
-                                   "21_Amitie","22_Raffina","23_Sig","24_Lemres","25_Feli","26_Klug",
-                                   "31_Pinkbean","32_Evan","33_Phantom","34_Angelic","35_Hoyoung","36_Lara",
-                                   "41_HelloKitty","42_MyMelody","43_Kuromi","44_Pocha","45_Purin","46_Cinnamoroll"};
+    private string[] spriteNames;
     private void Start()
     {
+        spriteNames = Resources.LoadAll<Sprite>("Characters/Icon").Select(s => s.name).ToArray();
+
         for (int i = 0; i < 24; i++)
         {
             // Sprite Atlas에서 특정 스프라이트를 가져오기
-            Sprite targetSprite = spriteAtlas.GetSprite(spriteName[i]);  // "SpriteName"은 스프라이트 이름
+            Sprite targetSprite = spriteAtlas.GetSprite(spriteNames[i]);  // "SpriteName"은 스프라이트 이름
             int x = i / 3;
             int y = i % 3;
             image[y * 8 + x].sprite = targetSprite;
